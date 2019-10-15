7b8ef62a9b0e25d91862cf4aa09d6a63af73a432
IagoFreire <iago_rocha@live.com>
2019-10-15 11:35:43 -0300

    add sucrase and nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.post('/users', (req, res) => {
-  return res.json();
+  return res.json({ ok: true});
 })
+
+export default routes;
