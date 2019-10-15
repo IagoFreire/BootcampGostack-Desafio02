27488902eb15a19be09d9aa039bd7dfb923c6f48
IagoFreire <iago_rocha@live.com>
2019-10-15 11:50:14 -0300

    config eslint, prettier and editorconfig

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.post('/users', (req, res) => {
-  return res.json({ ok: true});
-})
+  return res.json({ ok: true });
+});
 
 export default routes;
