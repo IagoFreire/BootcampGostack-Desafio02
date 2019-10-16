76c7216fb3bca359f552f37714909c506281669b
IagoFreire <iago_rocha@live.com>
2019-10-16 11:34:59 -0300

    config sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialeto: 'postgres',
+  host: 'localhost',
+  usernmae: 'postgres',
+  password: 'docker',
+  database: 'database',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
