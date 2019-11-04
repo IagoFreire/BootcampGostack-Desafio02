6b69ae597536a24b7904147bd06e3872d4184298
IagoFreire <iago_rocha@live.com>
2019-11-04 12:12:29 -0300

    add update user

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
@@ -24,8 +25,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, '787d7119fc4d833638ae20fe099ab37e', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '787d7119fc4d833638ae20fe099ab37e',
+  expiresIn: '7d',
+};
